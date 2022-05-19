9fe456ff55a377d5a0a8d8ed690c16de93e41a3c
HUNOR MORICZI <hm586@inlumine.ual.es>
2022-05-19 23:25:41 +0200

    nuevas imagenes + carta completa + stats + habilidades desde API

--- frontend/src/app/pokedex/pokedex.component.ts
+++ frontend/src/app/pokedex/pokedex.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../pokemon.service'; // Importamos el servicio de pokemon para poder usar sus métodos
 
 @Component({
   selector: 'app-pokedex',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokedexComponent implements OnInit {
 
-  constructor() { }
+  pokemons: any[] = []
+  private AllPokemons:any = this.pokemons
+  name: string | undefined
+  vistos: any[] = [1,4,6,7,16]
+
+  constructor(private http: PokemonService) { }
 
   ngOnInit(): void {
+    this.http.getAllPokemons()
+    .subscribe((response: any) => {
+      response.results.forEach((result: { name: string; }) => {
+        this.http.getPokemonByName(result.name)
+        .subscribe((uniqResponse: any) => {
+          this.pokemons.push(uniqResponse);
+          console.log(this.pokemons);
+        });
+      });
+    })
   }
 
+  searchPokemon(name: string) {
+    this.pokemons = this.AllPokemons.filter((pokemon: any) => {
+      return pokemon.name.toLowerCase().includes(name.toLowerCase());
+    })
+  }
+
+  //compruebo si el pokemon ya esta en la lista de vistos
+  comprobarVisto(pokemon: any){
+    if(this.vistos.includes(pokemon.id)){
+      return true
+    }else{
+      return false
+    }
+  }
 }
